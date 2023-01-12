c6489cf5456bbe079e1354d377fd5139e5710fe2
youssef labrahmi <ylabrahm@c2r6p8.1337.ma>
2023-01-12 22:18:45 +0100

    all

--- so_checkmap_2.c
+++ so_checkmap_2.c
@@ -6,7 +6,7 @@
 /*   By: ylabrahm <ylabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 11:51:34 by ylabrahm          #+#    #+#             */
-/*   Updated: 2023/01/12 20:06:53 by ylabrahm         ###   ########.fr       */
+/*   Updated: 2023/01/12 22:18:26 by ylabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void	ft_check_minim(t_args *vars)
 		i++;
 	}
 	if ((comps.c == 0) || (comps.e != 1) || (comps.p != 1))
-		my_exit("The map requirements aren't valid", 1, vars);
+		my_exit("The map requirements aren't valid.", 1, vars);
 }
 
 void	ft_check_rect(t_args *vars, const char *map)
@@ -78,7 +78,7 @@ void	ft_check_rect(t_args *vars, const char *map)
 	last_line = vars->map2d[lines - 1];
 	i = ft_strlen(last_line);
 	if (last_line[i - 1] == '\n')
-		my_exit("The map format is not valid", 1, vars);
+		my_exit("The map format is not valid.", 1, vars);
 	i = 0;
 	ret = 0;
 	curr = ft_strlen(vars->map2d[i]);
@@ -91,7 +91,7 @@ void	ft_check_rect(t_args *vars, const char *map)
 	}
 	ret += 1 * ((ft_strlen(vars->map2d[i]) + 1) != curr);
 	if (ret != 0)
-		my_exit("The map format is not valid", 1, vars);
+		my_exit("The map format is not valid.", 1, vars);
 }
 
 void	ft_check_walls(t_args *vars, const char *map)
@@ -99,34 +99,53 @@ void	ft_check_walls(t_args *vars, const char *map)
 	int	lines;
 	int	ret;
 	int	i;
-	int	j;
-
-	ret = 0;
-	
+	int	j;	
 	
 	i = 0;
 	while (vars->map2d[0][i] != '\n')
 		if (vars->map2d[0][i++] != '1')
-			my_exit("The map is not valid (walls)", 1, vars);
-	// -
+			my_exit("The map is not valid.", 1, vars);
 	lines = (ft_countlines(map) - 1);
 	while (vars->map2d[lines][i] != '\0')
 		if (vars->map2d[lines][i++] != '1')
-			my_exit("The map is not valid (walls)", 1, vars);
-	// -
-	
+			my_exit("The map is not valid.", 1, vars);
 	i = 0;
 	while (i < lines + 1)
+		if (vars->map2d[i++][0] != '1')
+			my_exit("The map is not valid.", 1, vars);
+	i = 0;
+	ret = 0;
+	while (i < lines + 1)
 	{
-		printf("%c", vars->map2d[i][0]);
+		j = ft_strlen(vars->map2d[i]);
+		ret += 1 * ((i < lines) && (vars->map2d[i][j - 2] != '1'));
+		ret += 1 * ((i == lines) && (vars->map2d[i][j - 1] != '1'));
 		i++;
 	}
-	exit(0);
+	if (ret != 0)
+		my_exit("The map is not valid.", 1, vars);
+}
+
+void	ft_check_strange(t_args *vars)
+{
+	char	c;
+	int		i;
+	int		j;
 	
-
-
-
-
+	i = 0;
+	while (vars->map2d[i])
+	{
+		j = 0;
+		while (vars->map2d[i][j])
+		{
+			c = vars->map2d[i][j];
+			if (!(c == '1' || c == '0' || c == 'P' || c == 'E' || c == 'C' || c == '\n'))
+				my_exit("Strange character is present.", 1, vars);
+			j++;
+		}
+		i++;	
+	}
+	
 }
 
 void	ft_check_all(const char *map, t_args *vars)
@@ -134,6 +153,7 @@ void	ft_check_all(const char *map, t_args *vars)
 	ft_check_rect(vars, map);
 	ft_check_minim(vars);
 	ft_check_walls(vars, map);
+	ft_check_strange(vars);
 	if (!(ft_check_path(map)))
-		my_exit("There is no possible path in the map!", 1, vars);
+		my_exit("There is no possible path in the map.", 1, vars);
 }
